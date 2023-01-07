ce5290f74a466d43756dc30426d40817b5151dce
Yu Hsun Lee <andylee0914.tw@gmail.com>
2023-01-07 16:06:24 +0800

    add shlink again

---
+++ shlink/precache-manifest.14c063444d837a1a91dd6b8fb02c462d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c3e72ee4e934066e3ecb1b63369ee0",
+    "url": "/shlink/index.html"
+  },
+  {
+    "revision": "3a96207fcab3ccb655cd",
+    "url": "/shlink/static/css/2.1a3b829c.chunk.css"
+  },
+  {
+    "revision": "1c493222ba49fc1742d0",
+    "url": "/shlink/static/css/main.202ca573.chunk.css"
+  },
+  {
+    "revision": "3a96207fcab3ccb655cd",
+    "url": "/shlink/static/js/2.3a96207f.chunk.js"
+  },
+  {
+    "revision": "3370ce4f8c8851e6c19160f51136ad75",
+    "url": "/shlink/static/js/2.3a96207f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c493222ba49fc1742d0",
+    "url": "/shlink/static/js/main.1c493222.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/shlink/static/js/main.1c493222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41d9433b1b40052eeb2c",
+    "url": "/shlink/static/js/runtime~main.41d9433b.js"
+  }
+]);
